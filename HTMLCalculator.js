ee4758aae338799e9709702093fc452441230bed
Hyper Flux <68274755+HyperFluxYT@users.noreply.github.com>
2020-11-12 13:02:33 +0530

    Update HTMLCalculator.js

--- HTMLCalculator.js
+++ HTMLCalculator.js
@@ -1,438 +1,334 @@
 var isAnswer = false;
+var displayField = document.getElementById("displayfield");
 function checkRefresh() {
-  return document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer;
+  return displayField.value == "0" || displayField.value == "NaN" || isAnswer;
 }
 function num1click() {
-  if (
-    checkRefresh()
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "1";
+  displayField.value = displayField.value + "1";
   isAnswer = false;
 }
 function num2click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "2";
+  displayField.value = displayField.value + "2";
   isAnswer = false;
 }
 function num3click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "3";
+  displayField.value = displayField.value + "3";
   isAnswer = false;
 }
 function num4click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "4";
+  displayField.value = displayField.value + "4";
   isAnswer = false;
 }
 function num5click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "5";
+  displayField.value = displayField.value + "5";
   isAnswer = false;
 }
 function num6click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "6";
+  displayField.value = displayField.value + "6";
   isAnswer = false;
 }
 function num7click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "7";
+  displayField.value = displayField.value + "7";
   isAnswer = false;
 }
 function num8click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "8";
+  displayField.value = displayField.value + "8";
   isAnswer = false;
 }
 function num9click() {
-  if (
-    document.getElementById("displayfield").value == "0" ||
-    document.getElementById("displayfield").value == "NaN" ||
-    isAnswer
-  ) {
-    document.getElementById("displayfield").value = null;
+  if (checkRefresh()) {
+    displayField.value = null;
   }
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + "9";
+  displayField.value = displayField.value + "9";
   isAnswer = false;
 }
 function num0click() {
-  if (document.getElementById("displayfield").value == "NaN" || isAnswer) {
-    document.getElementById("displayfield").value = null;
+  if (displayField.value == "NaN" || isAnswer) {
+    displayField.value = null;
   }
-  if (document.getElementById("displayfield").value != "0") {
-    document.getElementById("displayfield").value =
-      document.getElementById("displayfield").value + "0";
+  if (displayField.value != "0") {
+    displayField.value = displayField.value + "0";
   }
   isAnswer = false;
 }
 function negate() {
-  if (document.getElementById("displayfield").value != "0") {
-    if (document.getElementById("displayfield").value.includes("-", 0)) {
-      document.getElementById("displayfield").value = Math.abs(
-        parseFloat(document.getElementById("displayfield").value)
-      );
+  if (displayField.value != "0") {
+    if (displayField.value.includes("-", 0)) {
+      displayField.value = Math.abs(parseFloat(displayField.value));
     } else {
-      document.getElementById("displayfield").value =
-        "-" + document.getElementById("displayfield").value;
+      displayField.value = "-" + displayField.value;
     }
   }
 }
 function cleardisplayfield() {
-  document.getElementById("displayfield").value = "0";
+  displayField.value = "0";
 }
 function decimalpoint() {
   equals();
-  document.getElementById("displayfield").value =
-    document.getElementById("displayfield").value + ".";
+  displayField.value = displayField.value + ".";
 }
 function bckspace() {
-  document.getElementById("displayfield").value = document
+  displayField.value = document
     .getElementById("displayfield")
-    .value.slice(0, document.getElementById("displayfield").value.length - 1);
-  if (document.getElementById("displayfield").value == "") {
-    document.getElementById("displayfield").value = "0";
+    .value.slice(0, displayField.value.length - 1);
+  if (displayField.value == "") {
+    displayField.value = "0";
   }
 }
 function reciprocal() {
   equals();
-  let val1 = parseFloat(document.getElementById("displayfield").value);
+  let val1 = parseFloat(displayField.value);
   let val2 = 1 / val1;
-  document.getElementById("displayfield").value = val2;
+  displayField.value = val2;
   isAnswer = true;
 }
 function divisionf() {
   equals();
   isAnswer = false;
-  if (
-    document.getElementById("displayfield").value != "0" &&
-    !document.getElementById("displayfield").value.includes("÷", 0)
-  ) {
-    document.getElementById("displayfield").value =
-      document.getElementById("displayfield").value + "÷";
+  if (displayField.value != "0" && !displayField.value.includes("÷", 0)) {
+    displayField.value = displayField.value + "÷";
   }
 }
 function multiplicationf() {
   equals();
   isAnswer = false;
-  if (
-    document.getElementById("displayfield").value != "0" &&
-    !document.getElementById("displayfield").value.includes("×", 0)
-  ) {
-    document.getElementById("displayfield").value =
-      document.getElementById("displayfield").value + "×";
+  if (displayField.value != "0" && !displayField.value.includes("×", 0)) {
+    displayField.value = displayField.value + "×";
   }
 }
 function subtract() {
   equals();
   isAnswer = false;
-  if (
-    document.getElementById("displayfield").value != "0" &&
-    !document.getElementById("displayfield").value.includes("-", 0)
-  ) {
-    document.getElementById("displayfield").value =
-      document.getElementById("displayfield").value + "-";
+  if (displayField.value != "0" && !displayField.value.includes("-", 0)) {
+    displayField.value = displayField.value + "-";
   }
 }
 function add() {
   equals();
   isAnswer = false;
-  if (
-    document.getElementById("displayfield").value != "0" &&
-    !document.getElementById("displayfield").value.includes("+", 0)
-  ) {
-    document.getElementById("displayfield").value =
-      document.getElementById("displayfield").value + "+";
+  if (displayField.value != "0" && !displayField.value.includes("+", 0)) {
+    displayField.value = displayField.value + "+";
   }
 }
 function equals() {
   if (
-    document.getElementById("displayfield").value.includes("+", 0) &&
+    displayField.value.includes("+", 0) &&
     document
       .getElementById("displayfield")
-      .value.charAt(document.getElementById("displayfield").value.length - 1) !=
-      "+"
+      .value.charAt(displayField.value.length - 1) != "+"
   ) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("+", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("+", 0))
     );
     let val2 = parseFloat(
       document
         .getElementById("displayfield")
         .value.slice(
-          document.getElementById("displayfield").value.indexOf("+", 0),
-          document.getElementById("displayfield").value.length
+          displayField.value.indexOf("+", 0),
+          displayField.value.length
         )
     );
     let val3 = val1 + val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
-  if (document.getElementById("displayfield").value.includes("-", 0)) {
+  if (displayField.value.includes("-", 0)) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("-", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("-", 0))
     );
     let val2 = Math.abs(
       parseFloat(
         document
           .getElementById("displayfield")
           .value.slice(
-            document.getElementById("displayfield").value.indexOf("-", 0),
-            document.getElementById("displayfield").value.length
+            displayField.value.indexOf("-", 0),
+            displayField.value.length
           )
       )
     );
     let val3 = val1 - val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
-  if (document.getElementById("displayfield").value.includes("÷", 0)) {
+  if (displayField.value.includes("÷", 0)) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("÷", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("÷", 0))
     );
     let val2 = parseFloat(
       document
         .getElementById("displayfield")
         .value.slice(
-          document.getElementById("displayfield").value.indexOf("÷", 0) + 1,
-          document.getElementById("displayfield").value.length
+          displayField.value.indexOf("÷", 0) + 1,
+          displayField.value.length
         )
     );
     let val3 = val1 / val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
-  if (document.getElementById("displayfield").value.includes("×", 0)) {
+  if (displayField.value.includes("×", 0)) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("×", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("×", 0))
     );
     let val2 = parseFloat(
       document
         .getElementById("displayfield")
         .value.slice(
-          document.getElementById("displayfield").value.indexOf("×", 0) + 1,
-          document.getElementById("displayfield").value.length
+          displayField.value.indexOf("×", 0) + 1,
+          displayField.value.length
         )
     );
     let val3 = val1 * val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
 }
 function equalsbutton() {
   if (
-    document.getElementById("displayfield").value.includes("+", 0) &&
+    displayField.value.includes("+", 0) &&
     document
       .getElementById("displayfield")
-      .value.charAt(document.getElementById("displayfield").value.length - 1) !=
-      "+"
+      .value.charAt(displayField.value.length - 1) != "+"
   ) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("+", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("+", 0))
     );
     let val2 = parseFloat(
       document
         .getElementById("displayfield")
         .value.slice(
-          document.getElementById("displayfield").value.indexOf("+", 0),
-          document.getElementById("displayfield").value.length
+          displayField.value.indexOf("+", 0),
+          displayField.value.length
         )
     );
     let val3 = val1 + val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
-  if (document.getElementById("displayfield").value.includes("-", 0)) {
+  if (displayField.value.includes("-", 0)) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("-", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("-", 0))
     );
     let val2 = Math.abs(
       parseFloat(
         document
           .getElementById("displayfield")
           .value.slice(
-            document.getElementById("displayfield").value.indexOf("-", 0),
-            document.getElementById("displayfield").value.length
+            displayField.value.indexOf("-", 0),
+            displayField.value.length
           )
       )
     );
     let val3 = val1 - val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
-  if (document.getElementById("displayfield").value.includes("÷", 0)) {
+  if (displayField.value.includes("÷", 0)) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("÷", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("÷", 0))
     );
     let val2 = parseFloat(
       document
         .getElementById("displayfield")
         .value.slice(
-          document.getElementById("displayfield").value.indexOf("÷", 0) + 1,
-          document.getElementById("displayfield").value.length
+          displayField.value.indexOf("÷", 0) + 1,
+          displayField.value.length
         )
     );
     let val3 = val1 / val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
-  if (document.getElementById("displayfield").value.includes("×", 0)) {
+  if (displayField.value.includes("×", 0)) {
     let val1 = parseFloat(
       document
         .getElementById("displayfield")
-        .value.slice(
-          0,
-          document.getElementById("displayfield").value.indexOf("×", 0)
-        )
+        .value.slice(0, displayField.value.indexOf("×", 0))
     );
     let val2 = parseFloat(
       document
         .getElementById("displayfield")
         .value.slice(
-          document.getElementById("displayfield").value.indexOf("×", 0) + 1,
-          document.getElementById("displayfield").value.length
+          displayField.value.indexOf("×", 0) + 1,
+          displayField.value.length
         )
     );
     let val3 = val1 * val2;
-    document.getElementById("displayfield").value = val3;
+    displayField.value = val3;
   }
   isAnswer = false;
 }
 function square() {
   equals();
-  let val1 = parseFloat(document.getElementById("displayfield").value);
+  let val1 = parseFloat(displayField.value);
   let val2 = Math.pow(val1, 2);
-  document.getElementById("displayfield").value = val2;
+  displayField.value = val2;
   isAnswer = false;
 }
 function sqrtt() {
   equals();
-  let val1 = parseFloat(document.getElementById("displayfield").value);
+  let val1 = parseFloat(displayField.value);
   let val2 = Math.pow(val1, 1 / 2);
-  document.getElementById("displayfield").value = val2;
+  displayField.value = val2;
   isAnswer = false;
 }
 function percentage() {
   divisionf();
-  document.getElementById("displayfield").value =
-    parseFloat(document.getElementById("displayfield").value) * 100;
+  displayField.value = parseFloat(displayField.value) * 100;
 }
 function NaNcheck() {
-  if ((document.getElementById("displayfield").value = "NaN")) {
-    document.getElementById("displayfield").value = null;
+  if ((displayField.value = "NaN")) {
+    displayField.value = null;
   }
 }
 function cefunc() {
-  if (document.getElementById("displayfield").value.includes("+", 0)) {
-    document.getElementById("displayfield").value = document
+  if (displayField.value.includes("+", 0)) {
+    displayField.value = document
       .getElementById("displayfield")
-      .value.slice(
-        0,
-        document.getElementById("displayfield").value.indexOf("+", 0) + 1
-      );
-  } else if (document.getElementById("displayfield").value.includes("-", 0)) {
-    document.getElementById("displayfield").value = document
+      .value.slice(0, displayField.value.indexOf("+", 0) + 1);
+  } else if (displayField.value.includes("-", 0)) {
+    displayField.value = document
       .getElementById("displayfield")
-      .value.slice(
-        0,
-        document.getElementById("displayfield").value.indexOf("-", 0) + 1
-      );
-  } else if (document.getElementById("displayfield").value.includes("×", 0)) {
-    document.getElementById("displayfield").value = document
+      .value.slice(0, displayField.value.indexOf("-", 0) + 1);
+  } else if (displayField.value.includes("×", 0)) {
+    displayField.value = document
       .getElementById("displayfield")
-      .value.slice(
-        0,
-        document.getElementById("displayfield").value.indexOf("×", 0) + 1
-      );
-  } else if (document.getElementById("displayfield").value.includes("÷", 0)) {
-    document.getElementById("displayfield").value = document
+      .value.slice(0, displayField.value.indexOf("×", 0) + 1);
+  } else if (displayField.value.includes("÷", 0)) {
+    displayField.value = document
       .getElementById("displayfield")
-      .value.slice(
-        0,
-        document.getElementById("displayfield").value.indexOf("÷", 0) + 1
-      );
+      .value.slice(0, displayField.value.indexOf("÷", 0) + 1);
   } else {
     cleardisplayfield();
   }
